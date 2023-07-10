4668c9418c8e9125145fd5413d8d637a663a70bc
David Mark Clements <david.mark.clements@gmail.com>
2023-07-10 16:05:13 +0200

    Update index.js

--- solutions/1/exercise-5/index.js
+++ solutions/1/exercise-5/index.js
@@ -1,10 +1,29 @@
-import Hypercore from 'hypercore'
+import Corestore from 'corestore'
+import Hyperswarm from 'hyperswarm'
 
 // docs.holepunch.to for more info
 
-const core = new Hypercore('./core')
+const storePath = process.argv[2] || './store'
+const coreKey = process.argv[3]
+const store = new Corestore(storePath)
+const swarm = new Hyperswarm()
+
+const core = coreKey
+  ? store.get({ key: Buffer.from(coreKey, 'hex') })
+  : store.get({ name: 'local-chat' })
 
 await core.ready()
+
+const topic = Buffer.alloc(32).fill('plan-b-mathias')
+swarm.join(topic)
+swarm.on('connection', function (connection) {
+  console.log('new connection from', connection.remotePublicKey.toString('hex'))
+  store.replicate(connection)
+})
+
+core.on('append', function () {
+  console.log('new messages appended...', core.length)
+})
 
 console.log(core.length, '<-- core.length')
 console.log(core.key.toString('hex'), '<-- the public key')
